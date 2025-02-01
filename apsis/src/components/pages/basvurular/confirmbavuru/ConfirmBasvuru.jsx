668bbb835324f886b6e66997dcc062d5581fe0bd
Serhat Peker <156471436+Goshawk256@users.noreply.github.com>
2025-02-01 15:09:18 +0300

    Update: Refactor ConfirmBasvuru component with enhanced styling and improved layout for radio button selection

--- apsis/src/components/pages/basvurular/confirmbavuru/ConfirmBasvuru.jsx
+++ apsis/src/components/pages/basvurular/confirmbavuru/ConfirmBasvuru.jsx
@@ -5,22 +5,27 @@ function ConfirmBasvuru() {
     const [selected, setSelected] = useState("");
 
     return (
-        <div className="radio-input">
-            {["Designer", "Student", "Teacher"].map((role, index) => {
-                const value = `value-${index + 1}`;
-                return (
-                    <label key={value} className={`label ${selected === value ? "selected" : ""}`}>
-                        <input
-                            type="radio"
-                            name="value-radio"
-                            value={value}
-                            checked={selected === value}
-                            onChange={() => setSelected(value)}
-                        />
-                        <p className="text">{role}</p>
-                    </label>
-                );
-            })}
+        <div className='confirm-basvuru'>
+            <h1 className='confirm-title'>Başvuru Kaydı Oluşturma</h1>
+            <h5 className='birim-title'>Başvurulacak Birim:</h5>
+            <div className="radio-input">
+                {["Dr.Öğr.Ü", "Doç. Dr.", "Prof. Dr."].map((role, index) => {
+                    const value = `value-${index + 1}`;
+                    return (
+                        <label key={value} className={`label ${selected === value ? "selected" : ""}`}>
+                            <input
+                                type="radio"
+                                name="value-radio"
+                                value={value}
+                                checked={selected === value}
+                                onChange={() => setSelected(value)}
+                            />
+                            <p className="text">{role}</p>
+                        </label>
+                    );
+                })}
+            </div>
+            <h5 className='atama-title'>Son Atama Tarihi:</h5>
         </div>
     );
 }
