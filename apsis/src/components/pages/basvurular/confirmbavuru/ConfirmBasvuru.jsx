fa92092b706c0bcb7ffa3d562bbe1e780b1d48a0
Serhat Peker <156471436+Goshawk256@users.noreply.github.com>
2025-02-01 15:53:27 +0300

    Update: Add date picker functionality to ConfirmBasvuru component with styling enhancements

--- apsis/src/components/pages/basvurular/confirmbavuru/ConfirmBasvuru.jsx
+++ apsis/src/components/pages/basvurular/confirmbavuru/ConfirmBasvuru.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import './ConfirmBasvuru.css';
+import dayjs from 'dayjs';
+import 'dayjs/locale/tr';  // Türkçe dil dosyasını içe aktarıyoruz
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 
 function ConfirmBasvuru() {
     const [selected, setSelected] = useState("");
@@ -26,6 +31,26 @@ function ConfirmBasvuru() {
                 })}
             </div>
             <h5 className='atama-title'>Son Atama Tarihi:</h5>
+            <div className='date-picker'>
+                <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="tr">
+                    <StaticDatePicker
+                        orientation="landscape"
+                        sx={{
+                            "& .Mui-selected": {
+                                backgroundColor: "#435dd8 !important", // Seçili tarihin arkaplan rengi
+                                color: "#fff !important", // Seçili tarihin metin rengi
+                            },
+                            "& .MuiPickersDay-root": {
+                                borderRadius: "10px", // Günleri daha yuvarlak yapar
+                                transition: "background-color 0.3s", // Geçiş efekti
+                            },
+                            "& .MuiPickersDay-root:hover": {
+                                backgroundColor: "#2d3750", // Gün üzerine gelince rengi değiştir
+                            }
+                        }}
+                    />
+                </LocalizationProvider>
+            </div>
         </div>
     );
 }
