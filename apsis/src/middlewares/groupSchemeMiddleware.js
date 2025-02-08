e3042780649441c302f531ed01a6cf08f5818ec2
Serhat Peker <156471436+Goshawk256@users.noreply.github.com>
2025-02-08 22:36:03 +0300

    Add group generation functions for various categories to improve project organization

--- apsis/src/middlewares/groupSchemeMiddleware.js
+++ apsis/src/middlewares/groupSchemeMiddleware.js
@@ -1,3 +1,47 @@
-const projects = {
+export const projectGroups = () => {
+    const headings = [];
+    for (let i = 1; i <= 29; i++) {
+        headings.push(`H${i}`);
+    }
+    return headings;
+};
 
-}
+export const thesisGroups = () => {
+    const headings = [];
+    for (let i = 1; i <= 29; i++) {
+        headings.push(`F${i}`);
+    }
+    return headings;
+};
+
+export const publicationGroups = () => {
+    const headings = [];
+    for (let i = 1; i <= 29; i++) {
+        headings.push(`A${i}`);
+    }
+    return headings;
+};
+
+export const artGroups = () => {
+    const headings = [];
+    for (let i = 1; i <= 29; i++) {
+        headings.push(`G${i}`);
+    }
+    return headings;
+};
+
+export const awardGroups = () => {
+    const headings = [];
+    for (let i = 1; i <= 29; i++) {
+        headings.push(`J${i}`);
+    }
+    return headings;
+};
+
+export const lessonGroups = () => {
+    const headings = [];
+    for (let i = 1; i <= 29; i++) {
+        headings.push(`E${i}`);
+    }
+    return headings;
+};
