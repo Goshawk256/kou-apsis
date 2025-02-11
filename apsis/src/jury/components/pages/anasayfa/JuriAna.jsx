548cced9762890ce31e09b7ba0fa1f9a634569d0
Serhat Peker <156471436+Goshawk256@users.noreply.github.com>
2025-02-11 18:39:07 +0300

    Add JuriHeader component with styles and integrate into JuriAna page

--- apsis/src/jury/components/pages/anasayfa/JuriAna.jsx
+++ apsis/src/jury/components/pages/anasayfa/JuriAna.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import JuriHeader from '../../header/JuriHeader'
 import './JuriAna.css'
 
 function JuriAna() {
     return (
         <div className='main-juriana'>
+            <JuriHeader />
             JuriAna
         </div>
     )
