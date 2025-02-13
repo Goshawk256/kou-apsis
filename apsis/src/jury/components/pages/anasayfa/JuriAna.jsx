4664e11b03ff6d09d6eec4cc99660b2ac6f89b5d
Serhat Peker <156471436+Goshawk256@users.noreply.github.com>
2025-02-13 14:18:06 +0300

    Enhance JuriAna component layout with new styles and structure for application types

--- apsis/src/jury/components/pages/anasayfa/JuriAna.jsx
+++ apsis/src/jury/components/pages/anasayfa/JuriAna.jsx
@@ -8,7 +8,46 @@ function JuriAna() {
     return (
 
         <div className="main-juriana">
-            selam ana
+            <div className='juriana-content' >
+                <div className='juriana-header' >
+                    <h1>Başvurular</h1>
+                </div>
+                <div className='juriana-columns' >
+                    <div className='juriana-column' >
+                        <div className='application-type-waiting' >
+                            <span>BEKLEYEN</span>
+                            <button>20</button>
+
+                        </div>
+
+                        <div className='application-content' >
+
+                        </div>
+                    </div>
+                    <div className='juriana-column' >
+                        <div className='application-type-success' >
+                            <span>ONAYLANAN</span>
+                            <button>20</button>
+
+                        </div>
+
+                        <div className='application-content' >
+
+                        </div>
+                    </div>
+                    <div className='juriana-column' >
+                        <div className='application-type-reject' >
+
+                            <span>REDDEDİLEN</span>
+                            <button>20</button>
+                        </div>
+
+                        <div className='application-content' >
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
