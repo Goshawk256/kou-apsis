7c29e697caf1b867f05985ad6200cbcc685328a0
Serhat Peker <156471436+Goshawk256@users.noreply.github.com>
2025-02-17 22:46:01 +0300

    Enhance BasvuruDetay component with a new table layout for content display; update styles for improved presentation

--- apsis/src/jury/components/pages/basvurudetay/BasvuruDetay.jsx
+++ apsis/src/jury/components/pages/basvurudetay/BasvuruDetay.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './BasvuruDetay.css'
 import previous from '../../../../assets/previous.png';
+import { FaPencilAlt } from 'react-icons/fa';
 import { useState, useEffect } from 'react';
 
 function BasvuruDetay({ onSelect }) {
-
 
     return (
         <div className='main-basvurudetay'>
@@ -16,14 +16,34 @@ function BasvuruDetay({ onSelect }) {
                 <div className='basvurudetay-header'>
                     <span className='user-header'>Başvuru Sahibi: Doç.Dr. SUHAP ŞAHİN</span>
                     <span className='user-date'>Başvuru Tarihi: 12.05.2021</span>
-
                 </div>
                 <div className='basvurudetay-inner'>
+                    <div className='outer-table'>
+                        <table className='basvurudetay-table'>
+                            <thead>
+                                <tr>
+                                    <th>İçerik Adı</th>
+                                    <th>İçerik Grubu</th>
+                                    <th>İçerik Puanı</th>
+                                    <th>Yüklenen Belge</th>
+                                    <th>İşlem</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Başvuru İçeriği Adı</td>
+                                    <td>A5</td>
+                                    <td>90</td>
+                                    <td><a href='https://www.google.com'>Belge</a></td>
+                                    <td> <button><FaPencilAlt /></button></td>
+                                </tr>
 
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-
     )
 }
 
