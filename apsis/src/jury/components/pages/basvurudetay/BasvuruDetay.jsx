26ad984d283be8f593b58c781b029d153efd1b01
Serhat Peker <156471436+Goshawk256@users.noreply.github.com>
2025-02-18 15:01:46 +0300

    Enhance BasvuruDetay component with new UI elements; update styles for table and pagination, and improve data handling for application details

--- apsis/src/jury/components/pages/basvurudetay/BasvuruDetay.jsx
+++ apsis/src/jury/components/pages/basvurudetay/BasvuruDetay.jsx
@@ -9,31 +9,44 @@ import All_Url from '../../../../url.js';
 function BasvuruDetay({ onSelect }) {
     const [selectedApplication, setSelectedApplication] = useState(null);
     const [loading, setLoading] = useState(true);
-
-
-
+    const [publications, setPublications] = useState([]);
+    const [projects, setProjects] = useState([]);
+    const [awards, setAwards] = useState([]);
+    const [lessons, setLessons] = useState([]);
+    const [thesis, setThesis] = useState([]);
+    const [artworks, setArtworks] = useState([]);
     useEffect(() => {
         const token = localStorage.getItem('accessToken');
-
         const fetchData = async () => {
             try {
 
                 const basvuruId = localStorage.getItem('selectedApplication');
-                console.log(basvuruId);
+
                 const response = await axios.post(`${All_Url.api_base_url}/jury/get-applications`, {}, {
                     headers: {
                         Authorization: `Bearer ${token}`
                     }
                 });
                 const preliminaryApps = response.data.data.preliminaryApplications || [];
-                console.log(preliminaryApps);
+
                 const scientificApps = response.data.data.scientificApplications || [];
-                console.log(scientificApps);
+
 
                 const selectedApplication = [...preliminaryApps, ...scientificApps].find(app => app.applicationId === basvuruId);
-
+                const publications = [...preliminaryApps, ...scientificApps].find(app => app.applicationId === basvuruId).publications;
+                const projects = [...preliminaryApps, ...scientificApps].find(app => app.applicationId === basvuruId).projects;
+                const awards = [...preliminaryApps, ...scientificApps].find(app => app.applicationId === basvuruId).awards;
+                const lessons = [...preliminaryApps, ...scientificApps].find(app => app.applicationId === basvuruId).lessons;
+                const thesis = [...preliminaryApps, ...scientificApps].find(app => app.applicationId === basvuruId).advisingThesis;
+                const artworks = [...preliminaryApps, ...scientificApps].find(app => app.applicationId === basvuruId).artworks;
+                setPublications(publications);
+                setProjects(projects);
+                setAwards(awards);
+                setLessons(lessons);
+                setThesis(thesis);
+                setArtworks(artworks);
                 setSelectedApplication(selectedApplication);
-                console.log(selectedApplication);
+
             } catch (error) {
                 console.error("Hata oluştu:", error);
             }
@@ -63,6 +76,35 @@ function BasvuruDetay({ onSelect }) {
                                 <span className='user-date'>Başvuru Tarihi: 12.05.2021</span>
                             </div>
                             <div className='basvurudetay-inner'>
+                                <div className='table-section' >
+                                    <button>Yayınlar</button>
+                                    <button>Dersler</button>
+                                    <button>Yönetilen Tezler</button>
+                                    <button>Proje Görevleri</button>
+                                    <button>Ödüller</button>
+                                    <button>Sanatsal Faaliyetler</button>
+                                </div>
+                                <div className="basvurudetay-main-row-2">
+                                    <input
+                                        style={{ color: 'gray' }}
+                                        type="text"
+
+
+                                        placeholder="Arama Sözcüğünüzü Giriniz..."
+                                        className="basvurudetay-search-input"
+                                    />
+
+
+                                    <div className="basvurudetay-pagination">
+                                        <button >
+                                            ‹
+                                        </button>
+                                        <span>1 / 2</span>
+                                        <button >
+                                            ›
+                                        </button>
+                                    </div>
+                                </div>
                                 <div className='outer-table'>
                                     <table className='basvurudetay-table'>
                                         <thead>
@@ -71,20 +113,18 @@ function BasvuruDetay({ onSelect }) {
                                                 <th>İçerik Grubu</th>
                                                 <th>İçerik Puanı</th>
                                                 <th>Yüklenen Belge</th>
-                                                <th>Başvuru Tipi</th>
                                                 <th>İşlem</th>
                                             </tr>
                                         </thead>
                                         <tbody>
+
                                             <tr>
                                                 <td>Başvuru İçeriği Adı</td>
                                                 <td>A5</td>
                                                 <td>90</td>
                                                 <td><a href='https://www.google.com'>Belge</a></td>
-                                                <td>Kadro Başvurusu</td>
-                                                <td> <button><FaPencilAlt /></button></td>
+                                                <td><button><FaPencilAlt /></button></td>
                                             </tr>
-
                                         </tbody>
                                     </table>
                                 </div>
