24e375735fb852e96b67a928bbfbd42e956f0303
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-02-19 13:24:39 +0300

    RectorHomePage ve RectorHeader bileşenlerinde kullanıcı arayüzü güncellemeleri yapıldı; sayfa geçişleri ve kullanıcı bilgisi alma işlevselliği eklendi.

--- apsis/src/rector/components/RectorHomePage.jsx
+++ apsis/src/rector/components/RectorHomePage.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import  {  useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+import RectorHeader from './header/RectorHeader'
+import RectorSidebar from './sidebar/RectorSidebar'
+
+
+
 
 function RectorHomePage() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [selectedPage, setSelectedPage] = useState('Ana Sayfa');
+
+
+
+
+    const renderContent = () => {
+        switch (selectedPage) {
+            case 'Ana Sayfa':
+                return <div>rector ana sayfa</div>;
+            case 'Basvurudetay':
+                return <div>rector basvurudetay</div>;
+            default:
+                return <div>rector ana sayfa</div>;
+        }
+    };
+
+    return (
+
+        <div className="main-homepage">
+            <RectorHeader />
+            <RectorSidebar onSelect={setSelectedPage} />
+            <div className="flexible-component">
+                <AnimatePresence mode="wait">
+                    <motion.div
+                        key={selectedPage}
+                        initial={{ opacity: 0, x: -50 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: 50 }}
+                        transition={{ duration: 0.3 }}
+                    >
+                        {renderContent()}
+                    </motion.div>
+                </AnimatePresence>
+            </div>
+        </div>
+
+    );
 }
 
-export default RectorHomePage
+export default RectorHomePage;
--- apsis/src/rector/components/header/RectorHeader.jsx
+++ apsis/src/rector/components/header/RectorHeader.jsx
@@ -1,12 +1,114 @@
-import React from 'react'
-import './RectorHeader.css'
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './RectorHeader.css';
+import logoLight from '../../../assets/unnamed.png';
+import logoutIcon from '../../../assets/log-out.svg'
+import { getUserInfoByUsername } from '../../../service/user.js';
 
 function RectorHeader() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
+
+    const [userInfo, setUserInfo] = useState(null);
+    const username = localStorage.getItem('username');
+
+    useEffect(() => {
+        setIsLoggedIn(!!username);
+    }, [username]);
+
+    const handleLogout = () => {
+        localStorage.clear();
+        setIsLoggedIn(false);
+        navigate('/');
+    };
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setIsLoading(true);
+
+
+                const response = await getUserInfoByUsername(username);
+
+                const userInfoData = response?.data;
+
+                if (userInfoData) {
+                    setUserInfo(userInfoData);
+                } else {
+                    console.error('User data not found.');
+                }
+            } catch (err) {
+                console.error('Error fetching data:', err);
+                console.error('Error fetching data');
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+        return () => {
+            setUserInfo(null);
+
+        };
+    }, [username]);
+
+
+
+
+    return (
+        isLoggedIn ? (
+            isLoading ? (
+                <div className="loading">
+
+                </div>
+            ) : (
+                <div className={`header-container`}>
+                    <div className="site-name">
+                        <img src={logoLight} alt="Logo" />
+                        <span className="shining-text">Kocaeli Üniversitesi Akademik Puan Sistemi(Jüri Portalı)</span>
+                    </div>
+                    <button className="inbox-btn">
+                        <svg viewBox="0 0 512 512" height="10" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"
+                            ></path>
+                        </svg>
+                        <span className="msg-count">0</span>
+                    </button>
+                    <div className="username">
+                        <span>Jüri {username} </span>
+                    </div>
+                    <div className="checkbox-wrapper-35">
+
+                        {
+                            /*
+                            <label htmlFor="themeSwitch">
+                                <span className="switch-x-toggletext">
+                                    <span className="switch-x-unchecked">
+                                        <span className="switch-x-hiddenlabel">Unchecked: </span>
+                                        Aydınlık
+                                    </span>
+                                    <span className="switch-x-checked">
+                                        <span className="switch-x-hiddenlabel">Checked: </span>
+                                        Karanlık
+                                    </span>
+                                </span>
+                                <span className="switch-x-text">Tema </span>
+                            </label>
+                            */
+                        }
+                    </div>
+                    <button onClick={handleLogout} className="logout-button">
+                        <img src={logoutIcon} alt="Logout" />
+                    </button>
+                </div>
+            )
+        ) : null
+    );
+
 }
 
-export default RectorHeader
+export default RectorHeader;
