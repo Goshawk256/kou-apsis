4e4b37e308eed0398a0dcdd0fef0099db65e8f29
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-02-19 16:15:29 +0300

    Enhance RectorAna layout with new column styles and adjust component structure for improved UI

--- apsis/src/rector/components/pages/anasayfa/RectorAna.jsx
+++ apsis/src/rector/components/pages/anasayfa/RectorAna.jsx
@@ -8,7 +8,7 @@ function RectorAna() {
         <div className='rectorana-column-1' >
           <div className='rectorana-column-1-row-1' >
             <span className='rector-header'>Mustafa Serhat Peker</span>
-            <span className='rector-desc'> Rektürlük Atama Sistemi Analiz Sayfası</span>
+            <span className='rector-desc'> Rektörlük Atama Sistemi Analiz Sayfası</span>
           </div>
           <div className='rectorana-column-1-row-2' >
             <div className='little-card'></div>
@@ -17,10 +17,10 @@ function RectorAna() {
           </div>
           <div className='rectorana-column-1-row-3' >
             <div className='r-c-1-r-3-c'>
-              <div className='value-1'></div>
+             
             </div>
             <div className='r-c-1-r-3-c'>
-              <div className='value-2'></div>
+             
             </div>
           </div>
         </div>
