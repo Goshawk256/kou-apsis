6793c35e393cd82466c643eca0b6d7996b8d3c89
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-02-21 02:37:36 +0300

    Add Lists and Applications components with styling and layout enhancements

--- apsis/src/rector/components/pages/applications/Applications.jsx
+++ apsis/src/rector/components/pages/applications/Applications.jsx
@@ -1,8 +1,215 @@
 import React from "react";
 import "./Applications.css";
+import user from "../../../../assets/user.png";
+import check from "../../../../assets/check.png";
 
 function Applications() {
-  return <div>Applications</div>;
+  const truncateText = (text) => {
+    return text.length > 25 ? text.substring(0, 22) + "..." : text;
+  };
+  return (
+    <div className="applications-main">
+      <h2 className="applications-header">Başvurular</h2>
+      <div className="applications-content">
+        <div className="content-column-1">
+          <div className="content-header">
+            <span>Ön Değerlendirme Başvuruları:</span>
+          </div>
+          <div className="content">
+            <li>
+              <img src={user} alt="" />
+              <span>
+                Başvuran Kişi: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvurulan Kadro: <br /> Doç. Dr.
+              </span>
+              <span>
+                İlgili Hakem: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvuru Durumu: <br />
+                Onaylandı
+              </span>
+              <button>
+                <img src={check} alt="" />
+              </button>
+            </li>
+            <li>
+              <img src={user} alt="" />
+              <span>
+                Başvuran Kişi: <br />
+                {truncateText("mustafaserhatpeker@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvurulan Kadro: <br /> Doç. Dr.
+              </span>
+              <span>
+                İlgili Hakem: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvuru Durumu: <br />
+                Reddedildi
+              </span>
+              <button>
+                <img src={check} alt="" />
+              </button>
+            </li>
+            <li>
+              <img src={user} alt="" />
+              <span>
+                Başvuran Kişi: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvurulan Kadro: <br /> Doç. Dr.
+              </span>
+              <span>
+                İlgili Hakem: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvuru Durumu: <br />
+                Onaylandı
+              </span>
+              <button>
+                <img src={check} alt="" />
+              </button>
+            </li>
+            <li>
+              {" "}
+              <img src={user} alt="" />
+              <span>
+                Başvuran Kişi: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvurulan Kadro: <br /> Doç. Dr.
+              </span>
+              <span>
+                İlgili Hakem: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvuru Durumu: <br />
+                Onaylandı
+              </span>
+              <button>
+                <img src={check} alt="" />
+              </button>
+            </li>
+          </div>
+          <div className="content-header">
+            <span>Kadro Başvuruları:</span>
+          </div>
+          <div className="content">
+            <li>
+              {" "}
+              <img src={user} alt="" />
+              <span>
+                Başvuran Kişi: <br />
+                {truncateText("hikmetcan.ozcan@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvurulan Kadro: <br /> Doç. Dr.
+              </span>
+              <span>
+                İlgili Hakem: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvuru Durumu: <br />
+                Onaylandı
+              </span>
+              <button>
+                <img src={check} alt="" />
+              </button>
+            </li>
+
+            <li>
+              {" "}
+              <img src={user} alt="" />
+              <span>
+                Başvuran Kişi: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvurulan Kadro: <br /> Doç. Dr.
+              </span>
+              <span>
+                İlgili Hakem: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvuru Durumu: <br />
+                Onaylandı
+              </span>
+              <button>
+                <img src={check} alt="" />
+              </button>
+            </li>
+            <li>
+              {" "}
+              <img src={user} alt="" />
+              <span>
+                Başvuran Kişi: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvurulan Kadro: <br /> Doç. Dr.
+              </span>
+              <span>
+                İlgili Hakem: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvuru Durumu: <br />
+                Onaylandı
+              </span>
+              <button>
+                <img src={check} alt="" />
+              </button>
+            </li>
+            <li>
+              {" "}
+              <img src={user} alt="" />
+              <span>
+                Başvuran Kişi: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvurulan Kadro: <br /> Doç. Dr.
+              </span>
+              <span>
+                İlgili Hakem: <br />
+                {truncateText("suhapsahin@kocaeli.edu.tr")}
+              </span>
+              <span>
+                Başvuru Durumu: <br />
+                Onaylandı
+              </span>
+              <button>
+                <img src={check} alt="" />
+              </button>
+            </li>
+          </div>
+        </div>
+        <div className="content-column-2">
+          <div className="content-header-last">
+            <span>Juri Bilgisi:</span>
+          </div>
+          <div className="content-last">Görmek İçin Başvuruyu Seçiniz</div>
+          <div className="content-header-last">
+            <span>Juri Mesajı:</span>
+          </div>
+          <div className="content-last">Görmek İçin Başvuruyu Seçiniz</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Applications;
--- apsis/src/rector/components/pages/lists/Lists.jsx
+++ apsis/src/rector/components/pages/lists/Lists.jsx
@@ -1,8 +1,131 @@
 import React from "react";
 import "./Lists.css";
-
+import { FaTrashAlt, FaPencilAlt } from "react-icons/fa";
 function Lists() {
-  return <div>Lists</div>;
+  return (
+    <div className="lists-main">
+      <h2 className="lists-header">İlanlar</h2>
+      <button className="add-list-button">Yeni İlan</button>
+      <div className="lists-content">
+        <li>
+          <span>
+            <b>İlan Adı:</b> <br />
+            Kocaeli Üniversitesi Doçentlik İlanı
+          </span>
+          <span>
+            <b>İlan Kadrosu:</b> <br /> Doç. Dr.
+          </span>
+          <span>
+            <b>Bitiş Tarihi:</b> <br />
+            12.04.2025
+          </span>
+          <span>
+            <b>İlan Açıklaması:</b> <br />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            laborum sunt nihil odio. Doloribus, tempora earum. Non debitis sit
+            atque, voluptas commodi incidunt placeat et quis fugiat maxime ex
+            obcaecati.
+          </span>
+          <div className="list-buttons">
+            <button className="edit-list">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-list">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+        <li>
+          <span>
+            <b>İlan Adı:</b> <br />
+            Kocaeli Üniversitesi Doçentlik İlanı
+          </span>
+          <span>
+            <b>İlan Kadrosu:</b> <br /> Doç. Dr.
+          </span>
+          <span>
+            <b>Bitiş Tarihi:</b> <br />
+            12.04.2025
+          </span>
+          <span>
+            <b>İlan Açıklaması:</b> <br />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            laborum sunt nihil odio. Doloribus, tempora earum. Non debitis sit
+            atque, voluptas commodi incidunt placeat et quis fugiat maxime ex
+            obcaecati.
+          </span>
+          <div className="list-buttons">
+            <button className="edit-list">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-list">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+        <li>
+          <span>
+            <b>İlan Adı:</b> <br />
+            Kocaeli Üniversitesi Doçentlik İlanı
+          </span>
+          <span>
+            <b>İlan Kadrosu:</b> <br /> Doç. Dr.
+          </span>
+          <span>
+            <b>Bitiş Tarihi:</b> <br />
+            12.04.2025
+          </span>
+          <span>
+            <b>İlan Açıklaması:</b> <br />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            laborum sunt nihil odio. Doloribus, tempora earum. Non debitis sit
+            atque, voluptas commodi incidunt placeat et quis fugiat maxime ex
+            obcaecati.
+          </span>
+          <div className="list-buttons">
+            <button className="edit-list">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-list">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+        <li>
+          <span>
+            <b>İlan Adı:</b> <br />
+            Kocaeli Üniversitesi Doçentlik İlanı
+          </span>
+          <span>
+            <b>İlan Kadrosu:</b> <br /> Doç. Dr.
+          </span>
+          <span>
+            <b>Bitiş Tarihi:</b> <br />
+            12.04.2025
+          </span>
+          <span>
+            <b>İlan Açıklaması:</b> <br />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            laborum sunt nihil odio. Doloribus, tempora earum. Non debitis sit
+            atque, voluptas commodi incidunt placeat et quis fugiat maxime ex
+            obcaecati.
+          </span>
+          <div className="list-buttons">
+            <button className="edit-list">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-list">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+      </div>
+    </div>
+  );
 }
 
 export default Lists;
