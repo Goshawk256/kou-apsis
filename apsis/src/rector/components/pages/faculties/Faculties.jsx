cbc4ad6b9e83454ed46e40ac97ac69df7a47a571
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-02-21 17:17:13 +0300

    Add Faculties and Juries components with styling and layout enhancements

--- apsis/src/rector/components/pages/faculties/Faculties.jsx
+++ apsis/src/rector/components/pages/faculties/Faculties.jsx
@@ -1,8 +1,79 @@
 import React from "react";
 import "./Faculties.css";
-
+import { FaTrashAlt, FaPencilAlt } from "react-icons/fa";
 function Faculties() {
-  return <div>Faculties</div>;
+  return (
+    <div className="faculties-main">
+      <h2 className="faculties-header">Fakülteler</h2>
+      <button className="add-faculty-button">Fakülte Ekle</button>
+      <div className="faculties-content">
+        <li>
+          <span>
+            <b>Fakülte Adı:</b> <br />
+            Mühendislik Fakültesi
+          </span>
+
+          <div className="faculty-buttons">
+            <button className="edit-faculty">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-faculty">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+        <li>
+          <span>
+            <b>Fakülte Adı:</b> <br />
+            Mühendislik Fakültesi
+          </span>
+
+          <div className="faculty-buttons">
+            <button className="edit-faculty">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-faculty">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+        <li>
+          <span>
+            <b>Fakülte Adı:</b> <br />
+            Mühendislik Fakültesi
+          </span>
+
+          <div className="faculty-buttons">
+            <button className="edit-faculty">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-faculty">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+        <li>
+          <span>
+            <b>Fakülte Adı:</b> <br />
+            Mühendislik Fakültesi
+          </span>
+
+          <div className="faculty-buttons">
+            <button className="edit-faculty">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-faculty">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+      </div>
+    </div>
+  );
 }
 
 export default Faculties;
--- apsis/src/rector/components/pages/juries/Juries.jsx
+++ apsis/src/rector/components/pages/juries/Juries.jsx
@@ -1,10 +1,107 @@
-import React from 'react'
-import './Juries.css'
-
+import React from "react";
+import "./Juries.css";
+import { FaTrashAlt, FaPencilAlt } from "react-icons/fa";
 function Juries() {
   return (
-    <div>Juries</div>
-  )
+    <div className="juries-main">
+      <h2 className="juries-header">Juriler</h2>
+      <button className="add-jury-button">Jüri Ekle</button>
+      <div className="juries-content">
+        <li>
+          <span>
+            <b>Jüri Adı:</b> <br />
+            suhapsahin@kocaeli.edu.tr
+          </span>
+          <span>
+            <b>Atandığı Fakülte:</b> <br /> Mühendislik Fakültesi
+          </span>
+          <span>
+            <b>Bitiş Tarihi:</b> <br />
+            12.04.2025
+          </span>
+
+          <div className="jury-buttons">
+            <button className="edit-jury">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-jury">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+        <li>
+          <span>
+            <b>Jüri Adı:</b> <br />
+            suhapsahin@kocaeli.edu.tr
+          </span>
+          <span>
+            <b>Atandığı Fakülte:</b> <br /> Mühendislik Fakültesi
+          </span>
+          <span>
+            <b>Bitiş Tarihi:</b> <br />
+            12.04.2025
+          </span>
+
+          <div className="jury-buttons">
+            <button className="edit-jury">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-jury">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+        <li>
+          <span>
+            <b>Jüri Adı:</b> <br />
+            suhapsahin@kocaeli.edu.tr
+          </span>
+          <span>
+            <b>Atandığı Fakülte:</b> <br /> Mühendislik Fakültesi
+          </span>
+          <span>
+            <b>Bitiş Tarihi:</b> <br />
+            12.04.2025
+          </span>
+
+          <div className="jury-buttons">
+            <button className="edit-jury">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-jury">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+        <li>
+          <span>
+            <b>Jüri Adı:</b> <br />
+            suhapsahin@kocaeli.edu.tr
+          </span>
+          <span>
+            <b>Atandığı Fakülte:</b> <br /> Mühendislik Fakültesi
+          </span>
+          <span>
+            <b>Bitiş Tarihi:</b> <br />
+            12.04.2025
+          </span>
+
+          <div className="jury-buttons">
+            <button className="edit-jury">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-jury">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+      </div>
+    </div>
+  );
 }
 
-export default Juries
+export default Juries;
