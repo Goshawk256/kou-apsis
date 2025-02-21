18685c8d62755f2fcbb0408e25d59ab8604c4e1b
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-02-21 22:51:57 +0300

    Update layout and styling in Applications and Lists components for better presentation

--- apsis/src/rector/components/pages/applications/Applications.jsx
+++ apsis/src/rector/components/pages/applications/Applications.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Applications.css";
 import user from "../../../../assets/user.png";
 import check from "../../../../assets/check.png";
@@ -7,207 +7,345 @@ function Applications() {
   const truncateText = (text) => {
     return text.length > 25 ? text.substring(0, 22) + "..." : text;
   };
+  const [isSelected, setIsSelected] = useState(false);
+
+  const selectApplication = () => {
+    setIsSelected(true);
+  };
   return (
     <div className="applications-main">
       <h2 className="applications-header">Başvurular</h2>
-      <div className="applications-content">
-        <div className="content-column-1">
-          <div className="content-header">
-            <span>Ön Değerlendirme Başvuruları:</span>
+      {isSelected ? (
+        "selam"
+      ) : (
+        <div className="applications-content">
+          <div className="content-column-2">
+            <div className="content-header-last">
+              <span>Filtreleme:</span>
+            </div>
+            <div className="content-last">Görmek İçin Başvuruyu Seçiniz</div>
           </div>
-          <div className="content">
-            <li>
-              <img src={user} alt="" />
-              <span>
-                Başvuran Kişi: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvurulan Kadro: <br /> Doç. Dr.
-              </span>
-              <span>
-                İlgili Hakem: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvuru Durumu: <br />
-                Onaylandı
-              </span>
-              <button>
-                <img src={check} alt="" />
-              </button>
-            </li>
-            <li>
-              <img src={user} alt="" />
-              <span>
-                Başvuran Kişi: <br />
-                {truncateText("mustafaserhatpeker@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvurulan Kadro: <br /> Doç. Dr.
-              </span>
-              <span>
-                İlgili Hakem: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvuru Durumu: <br />
-                Reddedildi
-              </span>
-              <button>
-                <img src={check} alt="" />
-              </button>
-            </li>
-            <li>
-              <img src={user} alt="" />
-              <span>
-                Başvuran Kişi: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvurulan Kadro: <br /> Doç. Dr.
-              </span>
-              <span>
-                İlgili Hakem: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvuru Durumu: <br />
-                Onaylandı
-              </span>
-              <button>
-                <img src={check} alt="" />
-              </button>
-            </li>
-            <li>
-              {" "}
-              <img src={user} alt="" />
-              <span>
-                Başvuran Kişi: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvurulan Kadro: <br /> Doç. Dr.
-              </span>
-              <span>
-                İlgili Hakem: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvuru Durumu: <br />
-                Onaylandı
-              </span>
-              <button>
-                <img src={check} alt="" />
-              </button>
-            </li>
-          </div>
-          <div className="content-header">
-            <span>Kadro Başvuruları:</span>
-          </div>
-          <div className="content">
-            <li>
-              {" "}
-              <img src={user} alt="" />
-              <span>
-                Başvuran Kişi: <br />
-                {truncateText("hikmetcan.ozcan@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvurulan Kadro: <br /> Doç. Dr.
-              </span>
-              <span>
-                İlgili Hakem: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvuru Durumu: <br />
-                Onaylandı
-              </span>
-              <button>
-                <img src={check} alt="" />
-              </button>
-            </li>
+          <div className="content-column-1">
+            <div className="content-header">
+              <span>Başvurular:</span>
+            </div>
+            <div className="content">
+              <li>
+                <img src={user} alt="" />
+                <span>
+                  Başvuran Kişi: <br />
+                  {truncateText("suhapsahin@kocaeli.edu.tr")}
+                </span>
+                <span>
+                  Başvurulan Kadro: <br /> Doç. Dr.
+                </span>
 
-            <li>
-              {" "}
-              <img src={user} alt="" />
-              <span>
-                Başvuran Kişi: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvurulan Kadro: <br /> Doç. Dr.
-              </span>
-              <span>
-                İlgili Hakem: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvuru Durumu: <br />
-                Onaylandı
-              </span>
-              <button>
-                <img src={check} alt="" />
-              </button>
-            </li>
-            <li>
-              {" "}
-              <img src={user} alt="" />
-              <span>
-                Başvuran Kişi: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvurulan Kadro: <br /> Doç. Dr.
-              </span>
-              <span>
-                İlgili Hakem: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvuru Durumu: <br />
-                Onaylandı
-              </span>
-              <button>
-                <img src={check} alt="" />
-              </button>
-            </li>
-            <li>
-              {" "}
-              <img src={user} alt="" />
-              <span>
-                Başvuran Kişi: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvurulan Kadro: <br /> Doç. Dr.
-              </span>
-              <span>
-                İlgili Hakem: <br />
-                {truncateText("suhapsahin@kocaeli.edu.tr")}
-              </span>
-              <span>
-                Başvuru Durumu: <br />
-                Onaylandı
-              </span>
-              <button>
-                <img src={check} alt="" />
-              </button>
-            </li>
+                <span>
+                  Başvuru Durumu: <br />
+                  Onaylandı
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Ön Değerlendirme
+                </span>
+                <button
+                  onClick={() => {
+                    selectApplication();
+                  }}
+                >
+                  <img src={check} alt="" />
+                </button>
+              </li>
+              <li>
+                <img src={user} alt="" />
+                <span>
+                  Başvuran Kişi: <br />
+                  {truncateText("mustafaserhatpeker@kocaeli.edu.tr")}
+                </span>
+                <span>
+                  Başvurulan Kadro: <br /> Doç. Dr.
+                </span>
+
+                <span>
+                  Başvuru Durumu: <br />
+                  Reddedildi
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Ön Değerlendirme
+                </span>
+                <button
+                  onClick={() => {
+                    selectApplication();
+                  }}
+                >
+                  <img src={check} alt="" />
+                </button>
+              </li>
+              <li>
+                <img src={user} alt="" />
+                <span>
+                  Başvuran Kişi: <br />
+                  {truncateText("suhapsahin@kocaeli.edu.tr")}
+                </span>
+                <span>
+                  Başvurulan Kadro: <br /> Doç. Dr.
+                </span>
+
+                <span>
+                  Başvuru Durumu: <br />
+                  Onaylandı
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Ön Değerlendirme
+                </span>
+                <button
+                  onClick={() => {
+                    selectApplication();
+                  }}
+                >
+                  <img src={check} alt="" />
+                </button>
+              </li>
+              <li>
+                {" "}
+                <img src={user} alt="" />
+                <span>
+                  Başvuran Kişi: <br />
+                  {truncateText("suhapsahin@kocaeli.edu.tr")}
+                </span>
+                <span>
+                  Başvurulan Kadro: <br /> Doç. Dr.
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Onaylandı
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Ön Değerlendirme
+                </span>
+                <button
+                  onClick={() => {
+                    selectApplication();
+                  }}
+                >
+                  <img src={check} alt="" />
+                </button>
+              </li>
+              <li>
+                <img src={user} alt="" />
+                <span>
+                  Başvuran Kişi: <br />
+                  {truncateText("suhapsahin@kocaeli.edu.tr")}
+                </span>
+                <span>
+                  Başvurulan Kadro: <br /> Doç. Dr.
+                </span>
+
+                <span>
+                  Başvuru Durumu: <br />
+                  Onaylandı
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Ön Değerlendirme
+                </span>
+                <button
+                  onClick={() => {
+                    selectApplication();
+                  }}
+                >
+                  <img src={check} alt="" />
+                </button>
+              </li>
+              <li>
+                <img src={user} alt="" />
+                <span>
+                  Başvuran Kişi: <br />
+                  {truncateText("mustafaserhatpeker@kocaeli.edu.tr")}
+                </span>
+                <span>
+                  Başvurulan Kadro: <br /> Doç. Dr.
+                </span>
+
+                <span>
+                  Başvuru Durumu: <br />
+                  Reddedildi
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Ön Değerlendirme
+                </span>
+                <button
+                  onClick={() => {
+                    selectApplication();
+                  }}
+                >
+                  <img src={check} alt="" />
+                </button>
+              </li>
+              <li>
+                <img src={user} alt="" />
+                <span>
+                  Başvuran Kişi: <br />
+                  {truncateText("suhapsahin@kocaeli.edu.tr")}
+                </span>
+                <span>
+                  Başvurulan Kadro: <br /> Doç. Dr.
+                </span>
+
+                <span>
+                  Başvuru Durumu: <br />
+                  Onaylandı
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Ön Değerlendirme
+                </span>
+                <button
+                  onClick={() => {
+                    selectApplication();
+                  }}
+                >
+                  <img src={check} alt="" />
+                </button>
+              </li>
+              <li>
+                {" "}
+                <img src={user} alt="" />
+                <span>
+                  Başvuran Kişi: <br />
+                  {truncateText("suhapsahin@kocaeli.edu.tr")}
+                </span>
+                <span>
+                  Başvurulan Kadro: <br /> Doç. Dr.
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Onaylandı
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Ön Değerlendirme
+                </span>
+                <button
+                  onClick={() => {
+                    selectApplication();
+                  }}
+                >
+                  <img src={check} alt="" />
+                </button>
+              </li>
+              <li>
+                <img src={user} alt="" />
+                <span>
+                  Başvuran Kişi: <br />
+                  {truncateText("suhapsahin@kocaeli.edu.tr")}
+                </span>
+                <span>
+                  Başvurulan Kadro: <br /> Doç. Dr.
+                </span>
+
+                <span>
+                  Başvuru Durumu: <br />
+                  Onaylandı
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Ön Değerlendirme
+                </span>
+                <button
+                  onClick={() => {
+                    selectApplication();
+                  }}
+                >
+                  <img src={check} alt="" />
+                </button>
+              </li>
+              <li>
+                <img src={user} alt="" />
+                <span>
+                  Başvuran Kişi: <br />
+                  {truncateText("mustafaserhatpeker@kocaeli.edu.tr")}
+                </span>
+                <span>
+                  Başvurulan Kadro: <br /> Doç. Dr.
+                </span>
+
+                <span>
+                  Başvuru Durumu: <br />
+                  Reddedildi
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Ön Değerlendirme
+                </span>
+                <button
+                  onClick={() => {
+                    selectApplication();
+                  }}
+                >
+                  <img src={check} alt="" />
+                </button>
+              </li>
+              <li>
+                <img src={user} alt="" />
+                <span>
+                  Başvuran Kişi: <br />
+                  {truncateText("suhapsahin@kocaeli.edu.tr")}
+                </span>
+                <span>
+                  Başvurulan Kadro: <br /> Doç. Dr.
+                </span>
+
+                <span>
+                  Başvuru Durumu: <br />
+                  Onaylandı
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Ön Değerlendirme
+                </span>
+                <button
+                  onClick={() => {
+                    selectApplication();
+                  }}
+                >
+                  <img src={check} alt="" />
+                </button>
+              </li>
+              <li>
+                {" "}
+                <img src={user} alt="" />
+                <span>
+                  Başvuran Kişi: <br />
+                  {truncateText("suhapsahin@kocaeli.edu.tr")}
+                </span>
+                <span>
+                  Başvurulan Kadro: <br /> Doç. Dr.
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Onaylandı
+                </span>
+                <span>
+                  Başvuru Durumu: <br />
+                  Ön Değerlendirme
+                </span>
+                <button
+                  onClick={() => {
+                    selectApplication();
+                  }}
+                >
+                  <img src={check} alt="" />
+                </button>
+              </li>
+            </div>
           </div>
         </div>
-        <div className="content-column-2">
-          <div className="content-header-last">
-            <span>Juri Bilgisi:</span>
-          </div>
-          <div className="content-last">Görmek İçin Başvuruyu Seçiniz</div>
-          <div className="content-header-last">
-            <span>Juri Mesajı:</span>
-          </div>
-          <div className="content-last">Görmek İçin Başvuruyu Seçiniz</div>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
--- apsis/src/rector/components/pages/lists/Lists.jsx
+++ apsis/src/rector/components/pages/lists/Lists.jsx
@@ -16,33 +16,8 @@ function Lists() {
             <b>İlan Kadrosu:</b> <br /> Doç. Dr.
           </span>
           <span>
-            <b>Bitiş Tarihi:</b> <br />
-            12.04.2025
-          </span>
-          <span>
-            <b>İlan Açıklaması:</b> <br />
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-            laborum sunt nihil odio. Doloribus, tempora earum. Non debitis sit
-            atque, voluptas commodi incidunt placeat et quis fugiat maxime ex
-            obcaecati.
-          </span>
-          <div className="list-buttons">
-            <button className="edit-list">
-              <FaPencilAlt />
-            </button>
-            <button className="delete-list">
-              {" "}
-              <FaTrashAlt />{" "}
-            </button>
-          </div>
-        </li>
-        <li>
-          <span>
-            <b>İlan Adı:</b> <br />
-            Kocaeli Üniversitesi Doçentlik İlanı
-          </span>
-          <span>
-            <b>İlan Kadrosu:</b> <br /> Doç. Dr.
+            <b>Fakülte:</b> <br />
+            Mühendislik Fakültesi
           </span>
           <span>
             <b>Bitiş Tarihi:</b> <br />
@@ -74,6 +49,10 @@ function Lists() {
             <b>İlan Kadrosu:</b> <br /> Doç. Dr.
           </span>
           <span>
+            <b>Fakülte:</b> <br />
+            Mühendislik Fakültesi
+          </span>
+          <span>
             <b>Bitiş Tarihi:</b> <br />
             12.04.2025
           </span>
@@ -101,6 +80,43 @@ function Lists() {
           </span>
           <span>
             <b>İlan Kadrosu:</b> <br /> Doç. Dr.
+          </span>
+          <span>
+            <b>Fakülte:</b> <br />
+            Mühendislik Fakültesi
+          </span>
+          <span>
+            <b>Bitiş Tarihi:</b> <br />
+            12.04.2025
+          </span>
+          <span>
+            <b>İlan Açıklaması:</b> <br />
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            laborum sunt nihil odio. Doloribus, tempora earum. Non debitis sit
+            atque, voluptas commodi incidunt placeat et quis fugiat maxime ex
+            obcaecati.
+          </span>
+          <div className="list-buttons">
+            <button className="edit-list">
+              <FaPencilAlt />
+            </button>
+            <button className="delete-list">
+              {" "}
+              <FaTrashAlt />{" "}
+            </button>
+          </div>
+        </li>
+        <li>
+          <span>
+            <b>İlan Adı:</b> <br />
+            Kocaeli Üniversitesi Doçentlik İlanı
+          </span>
+          <span>
+            <b>İlan Kadrosu:</b> <br /> Doç. Dr.
+          </span>
+          <span>
+            <b>Fakülte:</b> <br />
+            Mühendislik Fakültesi
           </span>
           <span>
             <b>Bitiş Tarihi:</b> <br />
