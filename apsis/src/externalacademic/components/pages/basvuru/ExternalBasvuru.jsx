5887431d50a7907584e647bbd3a4bc0d2b94e149
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-02-24 23:48:36 +0300

    Add MyApplications component to ExternalBasvuru; implement application table layout and actions

--- apsis/src/externalacademic/components/pages/basvuru/ExternalBasvuru.jsx
+++ apsis/src/externalacademic/components/pages/basvuru/ExternalBasvuru.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./ExternalBasvuru.css";
+import MyApplications from "./myapplications/MyApplications";
 
 function ExternalBasvuru() {
+  const [isMyApplications, setIsMyApplications] = useState(true);
   return (
     <div className="main-externalbasvuru">
       <div>selam</div>
