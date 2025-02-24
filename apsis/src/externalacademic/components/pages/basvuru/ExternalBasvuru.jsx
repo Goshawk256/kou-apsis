dc35c03c93d33e736c5cfa845e25c3452e06ec70
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-02-24 19:42:20 +0300

    Add ConfirmBasvuru and MyApplications components; update ExternalBasvuru to include a greeting message and add corresponding styles

--- apsis/src/externalacademic/components/pages/basvuru/ExternalBasvuru.jsx
+++ apsis/src/externalacademic/components/pages/basvuru/ExternalBasvuru.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./ExternalBasvuru.css";
 
 function ExternalBasvuru() {
-  return <div className="main-externalbasvuru"></div>;
+  return (
+    <div className="main-externalbasvuru">
+      <div>selam</div>
+    </div>
+  );
 }
 
 export default ExternalBasvuru;
---
+++ apsis/src/externalacademic/components/pages/basvuru/myapplications/MyApplications.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./MyApplications.css";
+
+function MyApplications() {
+  return (
+    <div className="main-myapplications-external">
+      <span className="external-header">Başvurularım</span>
+      <div></div>
+    </div>
+  );
+}
+
+export default MyApplications;
