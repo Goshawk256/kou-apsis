3b2717aa3e9efff6f5c617198e5c798d322d2ae6
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-02-28 00:42:36 +0300

    Enhance RightBar component layout and add category display for improved user experience

--- apsis/src/academic/components/rightbar/RightBar.jsx
+++ apsis/src/academic/components/rightbar/RightBar.jsx
@@ -6,6 +6,7 @@ function RightBar({ isOpen, onClose, givenGroup, givenId, from, refresh }) {
   const [requestUrl, setRequestUrl] = useState("");
   const [newGroup, setNewGroup] = useState("");
   const [idName, setIdName] = useState("");
+  const [name, setName] = useState("");
 
   useEffect(() => {
     switch (from) {
@@ -14,30 +15,35 @@ function RightBar({ isOpen, onClose, givenGroup, givenId, from, refresh }) {
           "https://apsis.kocaeli.edu.tr/api/academic/update-project-rank"
         );
         setIdName("projectId");
+        setName("Proje Düzenleme");
         break;
       case "publications":
         setRequestUrl(
           "https://apsis.kocaeli.edu.tr/api/academic/update-publication-rank"
         );
         setIdName("publicationId");
+        setName("Yayın Düzenleme");
         break;
       case "lessons":
         setRequestUrl(
           "https://apsis.kocaeli.edu.tr/api/academic/update-lesson-ranks"
         );
         setIdName("lessonId");
+        setName("Ders Düzenleme");
         break;
       case "awards":
         setRequestUrl(
           "https://apsis.kocaeli.edu.tr/api/academic/update-award-rank"
         );
         setIdName("awardId");
+        setName("Ödül Düzenleme");
         break;
       case "thesis":
         setRequestUrl(
           "https://apsis.kocaeli.edu.tr/api/academic/update-advising-thesis-rank"
         );
         setIdName("advisingThesisId");
+        setName("Tez Düzenleme");
         break;
       default:
         setRequestUrl("#");
@@ -85,25 +91,27 @@ function RightBar({ isOpen, onClose, givenGroup, givenId, from, refresh }) {
             X
           </button>
         </div>
+        <p>Kategori: {name}</p>
         <div className="right-bar-content">
-          <div className="right-bar-content-header">
-            <h1>{givenGroup}</h1>
+          <div className="content-r-1">
+            <div className="right-bar-content-header">
+              <span>Sistemin Atadığı Puan: {givenGroup}</span>
+            </div>
+
+            <div className="right-bar-content-body">
+              <label>Yeni Grup:</label>
+              <input
+                type="text"
+                value={newGroup}
+                onChange={(e) => setNewGroup(e.target.value)}
+                placeholder="Yeni grup giriniz"
+              />
+            </div>
+            <button className="update-btn" onClick={updateRank}>
+              Grubu Güncelle
+            </button>
           </div>
-          <div className="right-bar-content-body">
-            <p>Kategori: {from}</p>
-          </div>
-          <div className="right-bar-content-body">
-            <label>Yeni Grup:</label>
-            <input
-              type="text"
-              value={newGroup}
-              onChange={(e) => setNewGroup(e.target.value)}
-              placeholder="Yeni grup giriniz"
-            />
-          </div>
-          <button className="update-btn" onClick={updateRank}>
-            Grubu Güncelle
-          </button>
+          <div className="content-r-2"></div>
         </div>
       </div>
     </>
