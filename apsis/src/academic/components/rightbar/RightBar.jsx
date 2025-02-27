a1704bc2f24a0f3da9c3519983259cdbff8af3f5
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-02-27 21:56:16 +0300

    Refactor RightBar component to utilize givenGroup and givenId for improved display and functionality

--- apsis/src/academic/components/rightbar/RightBar.jsx
+++ apsis/src/academic/components/rightbar/RightBar.jsx
@@ -1,11 +1,31 @@
-import { useState } from "react";
-import PropTypes from "prop-types";
-import { useEffect } from "react";
-
 import "./RightBar.css";
 
-function RightBar({ givenId, givenGrup, givenPublicationTypeId }) {
-  return <></>;
+function RightBar({ isOpen, onClose, givenGroup, givenId }) {
+  return (
+    <>
+      <>
+        <div
+          className={`overlay ${isOpen ? "open" : ""}`}
+          onClick={onClose}
+        ></div>
+        <div className={`right-bar ${isOpen ? "open" : ""} `}>
+          <div className="right-bar-header">
+            <button className="close-btn" onClick={onClose}>
+              X
+            </button>
+          </div>
+          <div className="right-bar-content">
+            <div className="right-bar-content-header">
+              <h1>{givenGroup}</h1>
+            </div>
+            <div className="right-bar-content-body">
+              <p>{givenId}</p>
+            </div>
+          </div>
+        </div>
+      </>
+    </>
+  );
 }
 
 export default RightBar;
