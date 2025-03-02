bef37c88a26339108675bdaff3713f06f5c66db8
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-02 21:29:14 +0300

    Refactor token refresh logic in authMiddleware for improved clarity and error handling

--- apsis/src/middlewares/authMiddleware.js
+++ apsis/src/middlewares/authMiddleware.js
@@ -2,21 +2,18 @@ import All_Url from "../url";
 import axios from "axios";
 
 export const refreshTheToken = async () => {
-    try {
-        const response = await axios.post(
-            `${All_Url.api_base_url}/auth/refresh`,
-            { username: localStorage.getItem('username'), role: localStorage.getItem('role'), refreshToken: localStorage.getItem('refreshToken') },
+  try {
+    const response = await axios.post(`${All_Url.api_base_url}/auth/refresh`, {
+      username: localStorage.getItem("username"),
+      role: localStorage.getItem("role"),
+    });
 
-        );
-
-        if (response) {
-            localStorage.setItem('accessToken', response.data.accessToken);
-
-        }
-        else {
-            console.log('Token yenileme başarısız');
-        }
-    } catch (error) {
-        console.error('Token yenileme hatası:', error);
+    if (response) {
+      localStorage.setItem("accessToken", response.data.accessToken);
+    } else {
+      console.log("Token yenileme başarısız");
     }
-}
+  } catch (error) {
+    console.error("Token yenileme hatası:", error);
+  }
+};
