60ed1aba10101fe3cd623cb3bb0a4b784ee57bf5
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-04 00:15:43 +0300

    Add loading state to Faculties component for improved user experience

--- apsis/src/rector/components/pages/faculties/Faculties.jsx
+++ apsis/src/rector/components/pages/faculties/Faculties.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 function Faculties() {
   const [faculties, setFaculties] = useState({});
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchFaculties = async () => {
       const accessToken = localStorage.getItem("accessToken");
@@ -28,6 +29,8 @@ function Faculties() {
       } catch (error) {
         alert("Jüriler getirilirken hata oluştu!");
         console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -37,23 +40,38 @@ function Faculties() {
     <div className="faculties-main">
       <h2 className="faculties-header">Fakülteler</h2>
       {/* <button className="add-faculty-button">Fakülte Ekle</button> */}
-      <div className="juries-content">
-        {Object.entries(faculties).map(([key, facultyName]) => (
-          <li key={key}>
-            <span>
-              <b>Fakülte Adı:</b> <br /> {facultyName}
-            </span>
-            <div className="jury-buttons">
-              <button className="edit-jury">
-                <FaPencilAlt />
-              </button>
-              <button className="delete-jury">
-                <FaTrashAlt />
-              </button>
-            </div>
-          </li>
-        ))}
-      </div>
+
+      {loading ? (
+        <div className="hourglassBackground">
+          <div className="hourglassContainer">
+            <div className="hourglassCurves"></div>
+            <div className="hourglassCapTop"></div>
+            <div className="hourglassGlassTop"></div>
+            <div className="hourglassSand"></div>
+            <div className="hourglassSandStream"></div>
+            <div className="hourglassCapBottom"></div>
+            <div className="hourglassGlass"></div>
+          </div>
+        </div>
+      ) : (
+        <div className="juries-content">
+          {Object.entries(faculties).map(([key, facultyName]) => (
+            <li key={key}>
+              <span>
+                <b>Fakülte Adı:</b> <br /> {facultyName}
+              </span>
+              <div className="jury-buttons">
+                <button className="edit-jury">
+                  <FaPencilAlt />
+                </button>
+                <button className="delete-jury">
+                  <FaTrashAlt />
+                </button>
+              </div>
+            </li>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
