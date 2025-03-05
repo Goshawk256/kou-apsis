95df78f7abaa1bc966d76a3f00680211d1f88ed7
Goshawk256 <mustafaserhatpeker@gmail.com>
2025-03-05 16:25:23 +0300

    Add console log for response data in Projeler component for debugging purposes

--- apsis/src/academic/components/pages/projeler/Projeler.jsx
+++ apsis/src/academic/components/pages/projeler/Projeler.jsx
@@ -47,7 +47,7 @@ function Projeler() {
           },
         }
       );
-
+      console.log(response.data.data);
       setTableData(response.data.data || []);
       setFilteredData(response.data.data || []);
     } catch (error) {
